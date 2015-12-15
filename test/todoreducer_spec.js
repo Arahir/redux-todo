7fcfcc073d7d90328c515879b74db34f4f2ba7fd
Matthieu Faugere <matthieu.faugere@gmail.com>
2015-12-15 15:24:09 +0100

    add more tests

--- test/todoreducer_spec.js
+++ test/todoreducer_spec.js
@@ -11,20 +11,27 @@ describe('todo reducers', () => {
 
     done();
   });
-
-  it('defaulted to []', () => {
-    expect(store.getState()).to.deep.equal([]);
-  });
+  describe('initial params', () => {
+    it('should be defined', () => {
+      expect(store.getState()).to.exist;
+    })
+    it('should be an array', () => {
+      expect(Array.isArray(store.getState()).to.be.true)
+    });
+    it('should be defaulted to []', () => {
+      expect(store.getState()).to.deep.equal([]);
+    });
+  })
   describe('add todo', () => {
     it('should add a task when called with ADD_TODO', () => {
       store.dispatch({type: 'ADD_TODO'});
       expect(store.getState().length).to.equal(1);
     });
-    it('the new task should have the correct text', () => {
+    it('should have the correct text', () => {
       store.dispatch({type: 'ADD_TODO', text: 'coding'});
       expect(store.getState()[0].text).to.equal('coding');
     });
-    it('the id sould increment', () => {
+    it('should increment id', () => {
       store.dispatch({type: 'ADD_TODO', text: 'coding'});
       expect(store.getState()[0].id).to.equal(1);
     });
